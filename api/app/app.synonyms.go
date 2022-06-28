92ac78cccfbbaf7c8936bb098622e10e17136198
Nevill <nevill.dutt@gmail.com>
2022-06-28 15:23:08 +0800

    feat: implement synonyms API.

---
+++ api/app/app.synonyms.go
@@ -0,0 +1,330 @@
+package app
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/nevill/jiangjiang/api"
+)
+
+type Synonyms struct {
+	List   SynonymsList
+	Get    SynonymsGet
+	Create SynonymsCreate
+	Update SynonymsUpdate
+	Delete SynonymsDelete
+}
+
+// SynonymsList retrieves all available synonym sets for the engine.
+// see https://www.elastic.co/guide/en/app-search/current/synonyms.html#synonyms-get for details.
+type SynonymsList func(name string, o ...func(*SynonymsListRequest)) (*api.Response, error)
+
+func (h SynonymsList) WithContext(ctx context.Context) func(*SynonymsListRequest) {
+	return func(r *SynonymsListRequest) {
+		r.Context = ctx
+	}
+}
+
+func newSynonymsListFunc(tp api.Transport) SynonymsList {
+	return func(name string, o ...func(*SynonymsListRequest)) (*api.Response, error) {
+		r := SynonymsListRequest{
+			Request: api.Request{
+				Transport: tp,
+			},
+			Engine: name,
+		}
+		for _, f := range o {
+			f(&r)
+		}
+		return r.Do()
+	}
+}
+
+type SynonymsListRequest struct {
+	api.Request
+	Engine string
+}
+
+func (r SynonymsListRequest) Do() (*api.Response, error) {
+	path := fmt.Sprintf("/api/as/v1/engines/%s/synonyms", r.Engine)
+
+	req, err := api.NewRequest(http.MethodGet, path, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	if r.Context != nil {
+		req = req.WithContext(r.Context)
+	}
+
+	res, err := r.Transport.Perform(req)
+	if err != nil {
+		return nil, err
+	}
+
+	response := api.Response{
+		StatusCode: res.StatusCode,
+		Body:       res.Body,
+		Header:     res.Header,
+	}
+
+	return &response, nil
+}
+
+// SynonymsGet Retrieves a synonym set by ID.
+// see https://www.elastic.co/guide/en/app-search/current/synonyms.html#synonyms-list-one for details.
+type SynonymsGet func(name, id string, o ...func(*SynonymsGetRequest)) (*api.Response, error)
+
+func (h SynonymsGet) WithContext(ctx context.Context) func(*SynonymsGetRequest) {
+	return func(r *SynonymsGetRequest) {
+		r.Context = ctx
+	}
+}
+
+func newSynonymsGetFunc(tp api.Transport) SynonymsGet {
+	return func(name, id string, o ...func(*SynonymsGetRequest)) (*api.Response, error) {
+		r := SynonymsGetRequest{
+			Request: api.Request{
+				Transport: tp,
+			},
+			Engine: name,
+			Id:     id,
+		}
+		for _, f := range o {
+			f(&r)
+		}
+		return r.Do()
+	}
+}
+
+type SynonymsGetRequest struct {
+	api.Request
+	Engine string
+	Id     string
+}
+
+func (r SynonymsGetRequest) Do() (*api.Response, error) {
+	path := fmt.Sprintf("/api/as/v1/engines/%s/synonyms/%s", r.Engine, r.Id)
+	req, err := api.NewRequest(http.MethodGet, path, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	if r.Context != nil {
+		req = req.WithContext(r.Context)
+	}
+
+	res, err := r.Transport.Perform(req)
+	if err != nil {
+		return nil, err
+	}
+
+	response := api.Response{
+		StatusCode: res.StatusCode,
+		Body:       res.Body,
+		Header:     res.Header,
+	}
+
+	return &response, nil
+}
+
+// SynonymsCreate creates a new synonym set for the engine.
+// see https://www.elastic.co/guide/en/app-search/current/synonyms.html#synonyms-create for details.
+type SynonymsCreate func(name string, synonyms []string, o ...func(*SynonymsCreateRequest)) (*api.Response, error)
+
+func (h SynonymsCreate) WithContext(ctx context.Context) func(*SynonymsCreateRequest) {
+	return func(r *SynonymsCreateRequest) {
+		r.Context = ctx
+	}
+}
+
+func newSynonymsCreateFunc(tp api.Transport) SynonymsCreate {
+	return func(name string, synonyms []string, o ...func(*SynonymsCreateRequest)) (*api.Response, error) {
+		r := SynonymsCreateRequest{
+			Request: api.Request{
+				Transport: tp,
+			},
+			Engine:   name,
+			Synonyms: synonyms,
+		}
+		for _, f := range o {
+			f(&r)
+		}
+		return r.Do()
+	}
+}
+
+type SynonymsCreateRequest struct {
+	api.Request
+	Engine   string
+	Synonyms []string
+}
+
+func (r SynonymsCreateRequest) Do() (*api.Response, error) {
+	path := fmt.Sprintf("/api/as/v1/engines/%s/synonyms", r.Engine)
+
+	body, err := json.Marshal(map[string]interface{}{
+		"synonyms": r.Synonyms,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := api.NewRequest(http.MethodPost, path, bytes.NewReader(body))
+	if err != nil {
+		return nil, err
+	}
+
+	if r.Context != nil {
+		req = req.WithContext(r.Context)
+	}
+
+	if len(body) > 0 {
+		req.Header[api.HeaderContentType] = api.HeaderContentTypeJSON
+	}
+
+	res, err := r.Transport.Perform(req)
+	if err != nil {
+		return nil, err
+	}
+
+	response := api.Response{
+		StatusCode: res.StatusCode,
+		Body:       res.Body,
+		Header:     res.Header,
+	}
+
+	return &response, nil
+}
+
+// SynonymsUpdate updates a synonym set by ID.
+// see https://www.elastic.co/guide/en/app-search/current/synonyms.html#synonyms-update for details.
+type SynonymsUpdate func(name, id string, synonyms []string, o ...func(*SynonymsUpdateRequest)) (*api.Response, error)
+
+func (h SynonymsUpdate) WithContext(ctx context.Context) func(*SynonymsUpdateRequest) {
+	return func(r *SynonymsUpdateRequest) {
+		r.Context = ctx
+	}
+}
+
+func newSynonymsUpdateFunc(tp api.Transport) SynonymsUpdate {
+	return func(name, id string, synonyms []string, o ...func(*SynonymsUpdateRequest)) (*api.Response, error) {
+		r := SynonymsUpdateRequest{
+			Request: api.Request{
+				Transport: tp,
+			},
+			Engine:   name,
+			Id:       id,
+			Synonyms: synonyms,
+		}
+		for _, f := range o {
+			f(&r)
+		}
+		return r.Do()
+	}
+}
+
+type SynonymsUpdateRequest struct {
+	api.Request
+	Engine   string
+	Id       string
+	Synonyms []string
+}
+
+func (r SynonymsUpdateRequest) Do() (*api.Response, error) {
+	path := fmt.Sprintf("/api/as/v1/engines/%s/synonyms/%s", r.Engine, r.Id)
+
+	body, err := json.Marshal(map[string]interface{}{
+		"id":       r.Id,
+		"synonyms": r.Synonyms,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := api.NewRequest(http.MethodPut, path, bytes.NewReader(body))
+	if err != nil {
+		return nil, err
+	}
+
+	if r.Context != nil {
+		req = req.WithContext(r.Context)
+	}
+
+	if len(body) > 0 {
+		req.Header[api.HeaderContentType] = api.HeaderContentTypeJSON
+	}
+
+	res, err := r.Transport.Perform(req)
+	if err != nil {
+		return nil, err
+	}
+
+	response := api.Response{
+		StatusCode: res.StatusCode,
+		Body:       res.Body,
+		Header:     res.Header,
+	}
+
+	return &response, nil
+}
+
+// SynonymsUpdate deletes a synonym set by ID.
+// see https://www.elastic.co/guide/en/app-search/current/synonyms.html#synonyms-delete for details.
+type SynonymsDelete func(name, id string, o ...func(*SynonymsDeleteRequest)) (*api.Response, error)
+
+func (h SynonymsDelete) WithContext(ctx context.Context) func(*SynonymsDeleteRequest) {
+	return func(r *SynonymsDeleteRequest) {
+		r.Context = ctx
+	}
+}
+
+func newSynonymsDeleteFunc(tp api.Transport) SynonymsDelete {
+	return func(name, id string, o ...func(*SynonymsDeleteRequest)) (*api.Response, error) {
+		r := SynonymsDeleteRequest{
+			Request: api.Request{
+				Transport: tp,
+			},
+			Engine: name,
+			Id:     id,
+		}
+		for _, f := range o {
+			f(&r)
+		}
+		return r.Do()
+	}
+}
+
+type SynonymsDeleteRequest struct {
+	api.Request
+	Engine string
+	Id     string
+}
+
+func (r SynonymsDeleteRequest) Do() (*api.Response, error) {
+	path := fmt.Sprintf("/api/as/v1/engines/%s/synonyms/%s", r.Engine, r.Id)
+	req, err := api.NewRequest(http.MethodDelete, path, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	if r.Context != nil {
+		req = req.WithContext(r.Context)
+	}
+
+	res, err := r.Transport.Perform(req)
+	if err != nil {
+		return nil, err
+	}
+
+	response := api.Response{
+		StatusCode: res.StatusCode,
+		Body:       res.Body,
+		Header:     res.Header,
+	}
+
+	return &response, nil
+}
--- client_test.go
+++ client_test.go
@@ -3,6 +3,7 @@ package jiangjing
 import (
 	"encoding/json"
 	"net/http"
+	"strings"
 	"testing"
 )
 
@@ -156,4 +157,206 @@ func TestEngines(t *testing.T) {
 			t.Fatalf("Error parsing the response body: %s", err)
 		}
 	})
+}
+
+func TestSynonyms(t *testing.T) {
+	client, err := NewClient(Config{
+		Address:  address,
+		Username: username,
+		Password: password,
+	})
+	if err != nil {
+		t.Fatalf("Unexpected error: %s\n", err)
+	}
+
+	engine := "search-synonyms-testings"
+
+	{
+		// setup testing engine
+		resp, err := client.AppSearch.Engines.Create(engine)
+		defer resp.Body.Close()
+		if err != nil {
+			t.Fatalf("Unexpected error: %s\n", err)
+		}
+		if resp.StatusCode != http.StatusOK {
+			t.Fatalf("Expect to get status: %d, but got: %d\n", http.StatusOK, resp.StatusCode)
+		}
+	}
+
+	var synId string
+
+	t.Run("create a synonyms set", func(t *testing.T) {
+		synonyms := []string{
+			"ring", "rings", "fingerring", "fingerrings",
+		}
+		resp, err := client.AppSearch.Synonyms.Create(engine, synonyms)
+		defer resp.Body.Close()
+		if err != nil {
+			t.Fatalf("Unexpected error: %s\n", err)
+		}
+		if resp.StatusCode != http.StatusOK {
+			t.Fatalf("Expect to get status: %d, but got: %d\n", http.StatusOK, resp.StatusCode)
+		}
+
+		var r map[string]interface{}
+		if err := json.NewDecoder(resp.Body).Decode(&r); err != nil {
+			t.Fatalf("Error parsing the response body: %s", err)
+		}
+
+		switch id := r["id"].(type) {
+		case string:
+			if !strings.HasPrefix(id, "syn-") {
+				t.Fatalf("Expect to have syn- as prefix in ID, but got: %s in response.", id)
+			}
+			synId = id
+		default:
+			t.Fatal("Expect to have a field `id` returned in response.")
+		}
+
+		switch set := r["synonyms"].(type) {
+		case []interface{}:
+			if len(set) != len(synonyms) {
+				t.Fatalf("Expect to have: %s, but got: %s in response.", synonyms, set)
+			}
+		default:
+			t.Fatal("Expect to have a field `synonyms` returned in response.")
+		}
+	})
+
+	t.Run("update a synonyms set", func(t *testing.T) {
+		synonyms := []string{
+			"ring", "rings",
+		}
+		resp, err := client.AppSearch.Synonyms.Update(engine, synId, synonyms)
+		defer resp.Body.Close()
+		if err != nil {
+			t.Fatalf("Unexpected error: %s\n", err)
+		}
+		if resp.StatusCode != http.StatusOK {
+			t.Fatalf("Expect to get status: %d, but got: %d\n", http.StatusOK, resp.StatusCode)
+		}
+
+		var r map[string]interface{}
+		if err := json.NewDecoder(resp.Body).Decode(&r); err != nil {
+			t.Fatalf("Error parsing the response body: %s", err)
+		}
+
+		switch id := r["id"].(type) {
+		case string:
+			if id != synId {
+				t.Fatalf("Expect to have: %s, but got: %s in response.", synId, id)
+			}
+		default:
+			t.Fatal("Expect to have a field `id` returned in response.")
+		}
+
+		switch set := r["synonyms"].(type) {
+		case []interface{}:
+			if len(set) != len(synonyms) {
+				t.Fatalf("Expect to have: %s, but got: %s in response.", synonyms, set)
+			}
+		default:
+			t.Fatal("Expect to have a field `synonyms` returned in response.")
+		}
+	})
+
+	t.Run("Get a synonyms set", func(t *testing.T) {
+		resp, err := client.AppSearch.Synonyms.Get(engine, synId)
+		defer resp.Body.Close()
+		if err != nil {
+			t.Fatalf("Unexpected error: %s\n", err)
+		}
+		if resp.StatusCode != http.StatusOK {
+			t.Fatalf("Expect to get status: %d, but got: %d\n", http.StatusOK, resp.StatusCode)
+		}
+
+		var r map[string]interface{}
+		if err := json.NewDecoder(resp.Body).Decode(&r); err != nil {
+			t.Fatalf("Error parsing the response body: %s", err)
+		}
+
+		switch id := r["id"].(type) {
+		case string:
+			if synId != id {
+				t.Fatalf("Expect to get id: %s, but got: %s.", synId, id)
+			}
+		default:
+			t.Fatal("Expect to have a field `id` returned in response.")
+		}
+
+	})
+
+	t.Run("list synonyms set", func(t *testing.T) {
+		resp, err := client.AppSearch.Synonyms.List(engine)
+		defer resp.Body.Close()
+		if err != nil {
+			t.Fatalf("Unexpected error: %s\n", err)
+		}
+		if resp.StatusCode != http.StatusOK {
+			t.Fatalf("Expect to get status: %d, but got: %d\n", http.StatusOK, resp.StatusCode)
+		}
+
+		var r map[string]interface{}
+		if err := json.NewDecoder(resp.Body).Decode(&r); err != nil {
+			t.Fatalf("Error parsing the response body: %s", err)
+		}
+
+		var synonymsSet interface{}
+		switch results := r["results"].(type) {
+		case []interface{}:
+			if len(results) != 1 {
+				t.Fatalf("Expect to return one synonyms set.")
+			}
+			synonymsSet = results[0]
+		default:
+			t.Fatal("Expect to have a field `results` returned in response.")
+		}
+
+		switch set := synonymsSet.(type) {
+		case map[string]interface{}:
+			if set["id"] != synId {
+				t.Fatalf("Expect to have a synonyms id %s returned in response, but got: %s.", synId, set["id"])
+			}
+		default:
+			t.Fatalf("Expect to have a synonyms map returned in response, but got: %s.", set)
+		}
+	})
+
+	t.Run("delete a synonyms set", func(t *testing.T) {
+		resp, err := client.AppSearch.Synonyms.Delete(engine, synId)
+		defer resp.Body.Close()
+		if err != nil {
+			t.Fatalf("Unexpected error: %s\n", err)
+		}
+		if resp.StatusCode != http.StatusOK {
+			t.Fatalf("Expect to get status: %d, but got: %d\n", http.StatusOK, resp.StatusCode)
+		}
+
+		var r map[string]interface{}
+		if err := json.NewDecoder(resp.Body).Decode(&r); err != nil {
+			t.Fatalf("Error parsing the response body: %s", err)
+		}
+
+		switch deleted := r["deleted"].(type) {
+		case bool:
+			if !deleted {
+				t.Fatalf("Cannot delete synonyms set.")
+			}
+		default:
+			t.Fatal("Expect to have a field `deleted` returned in response.")
+		}
+
+	})
+
+	{
+		// remove testing engine
+		resp, err := client.AppSearch.Engines.Delete(engine)
+		defer resp.Body.Close()
+		if err != nil {
+			t.Fatalf("Unexpected error: %s\n", err)
+		}
+		if resp.StatusCode != http.StatusOK {
+			t.Fatalf("Expect to get status: %d, but got: %d\n", http.StatusOK, resp.StatusCode)
+		}
+	}
 }
